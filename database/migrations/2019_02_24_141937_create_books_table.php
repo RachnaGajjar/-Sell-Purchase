29ac28a6e956bfc59ea9c28d578662e838e56da7
rachna gajjar <rachnagajjar2112@gmail.com>
2019-02-24 22:24:50 +0530

    rachnaprojrct

---
+++ database/migrations/2019_02_24_141937_create_books_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->increments('id');
+            $table->bigInteger('isbn10');
+            $table->bigInteger('isbn13');
+            $table->string('title');
+            $table->string('author');
+            $table->string('image');
+            $table->text('description');
+            $table->float('mrp');
+            $table->string('publish_at');
+            $table->boolean('verified');
+            $table->timestamps();
+        }); 
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
