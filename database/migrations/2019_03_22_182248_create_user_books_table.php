84c059d9ba6134ac77fb9b8a266f301203ee2986
Harikrushna V Adiecha <hk.dev@autobits.in>
2019-03-24 15:47:59 +0530

    migration change

--- database/migrations/2019_03_22_182248_create_user_books_table.php
+++ database/migrations/2019_03_22_182248_create_user_books_table.php
@@ -13,7 +13,7 @@ class CreateUserBooksTable extends Migration
      */
     public function up()
     {
-        Schema::create('User_books', function (Blueprint $table) {
+        Schema::create('user_books', function (Blueprint $table) {
           
             $table->increments('id');
             $table->integer('user_id')->unsigned()->index();
